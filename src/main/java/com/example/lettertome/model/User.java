d2ace7cd028974c3d8e37065de9fae421132e436
dkrlto0819 <enqn_k@naver.com>
2019-06-07 00:11:27 +0900

    user model

--- src/main/java/com/example/lettertome/model/User.java
+++ src/main/java/com/example/lettertome/model/User.java
@@ -1,4 +1,20 @@
 package com.example.lettertome.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer user_id;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="email")
+    private String email;
+
 }
