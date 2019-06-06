27de8ec762c8877ec06add838c4e5785dbe5bcf5
dkrlto0819 <enqn_k@naver.com>
2019-06-06 21:10:22 +0900

    프로젝트 생성 및 데이터베이스 연결

---
+++ src/main/java/com/example/lettertome/LettertomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.lettertome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettertomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LettertomeApplication.class, args);
+    }
+
+}
