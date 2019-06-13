132deefe73571dbe70cb3014458a64133af98235
dkrlto0819 <enqn_k@naver.com>
2019-06-13 15:04:21 +0900

    view 연결시도

--- src/main/java/com/example/lettertome/LettertomeApplication.java
+++ src/main/java/com/example/lettertome/LettertomeApplication.java
@@ -2,12 +2,24 @@ package com.example.lettertome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jca.endpoint.GenericMessageEndpointFactory;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 public class LettertomeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LettertomeApplication.class, args);
+    }
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver
+                = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+        internalResourceViewResolver.setSuffix(".html");
+        return internalResourceViewResolver;
     }
 
 }
