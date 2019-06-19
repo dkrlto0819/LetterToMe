58aebb66e761dc967672576cb240c404ddd0718f
dkrlto0819 <enqn_k@naver.com>
2019-06-19 18:07:41 +0900

    게시판 json 데이터 매칭

--- Frontend/js/board.js
+++ Frontend/js/board.js
@@ -12,9 +12,35 @@ $(document).ready(function(){
     xhr.onreadystatechange = function (e) {
     if (xhr.readyState !== XMLHttpRequest.DONE) return;
         if(xhr.status === 200) { 
-            alert(xhr.responseText);
+            add_row(eval(xhr.responseText));
+            
         } else {
             alert("Error!");
         }
     };
 })
+
+function add_row(jsonData) {
+    var boardDetail = document.getElementById('board');
+    for(var i in jsonData){
+        row = boardDetail.insertRow(boardDetail.rows.length);  //추가할 행
+
+        title = row.insertCell(0);  //실제 행 추가 여기서의 숫자는 컬럼 수
+        see = row.insertCell(1);
+        remove = row.insertCell(2);
+        create_date = row.insertCell(3);
+        open_date = row.insertCell(4);
+
+        see.id="see"
+
+        title.innerHTML = "<td>"+(jsonData[i].content).substring(0, 20)+"</td>"
+        see.innerHTML = "<td>보기</td>"
+        remove.innerHTML = "<td>삭제</td>"
+        create_date.innerHTML = "<td>"+(jsonData[i].created_date).substring(0, 10)+"</td>"
+        open_date.innerHTML = "<td>"+(jsonData[i].open_date).substring(0, 10)+"</td>"
+    }
+  }
+
+  $(document).on("click","#see",function(event){
+    window.location.replace('http://localhost:8000/write.html'); 
+  });
