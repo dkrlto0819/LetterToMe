306b81c3b5bedaa8c9a7546df14826ff826dd161
dkrlto0819 <enqn_k@naver.com>
2019-06-20 01:36:17 +0900

    write formData 제작

--- Frontend/js/write.js
+++ Frontend/js/write.js
@@ -11,7 +11,34 @@ $(document).ready(function(){
     })
 
     submit.addEventListener('click', function(event){
-        
+
+        var formData = {
+            "date": $('#date').val(),
+            "content": $('#content').val()
+        }
+
+        console.log(formData);
+
+        // $.ajax({
+        //     url : apiurl,
+        //     type : "POST",
+        //     data : JSON.stringify(formData),
+        //     contentType : "application/json; charset=UTF-8",
+        //     async: false,
+        //     success : function(data) {
+        //         if(data=="" || undefined || null){
+        //             alert("저장되었습니다.");
+        //             window.location.replace('http://localhost:8000/write.html'); 
+        //         }
+        //         else{
+        //             console.log(data);
+        //             add_row(eval(data));
+        //         }
+        //     },
+        //     error : function(xhr, status, error) {
+        //         alert("에러발생");
+        //     }
+        // })
     })
 
 })
